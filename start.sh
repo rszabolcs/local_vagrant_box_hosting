a6b3fbc80cc47767d20d179552f932660be6e241
root <root@jenkins02.hq.firespring.com>
2015-11-24 08:57:09 -0600

    Fixed path issue

--- start.sh
+++ start.sh
@@ -9,6 +9,6 @@ sed -re "s@\\\$cfg_port@$cfg_port@g" -e "s@\\\$cfg_hostname@$cfg_hostname@g" con
 echo 
 echo "Starting nginx-vagrant-boxes"
 docker run -d -p $cfg_port:$cfg_port \
-  -v /root/nginx_vagrant_boxes/html:/usr/share/nginx/html:ro \
-  -v /root/nginx_vagrant_boxes/conf.d/:/etc/nginx/conf.d/:ro \
+  -v $cfg_path/html:/usr/share/nginx/html:ro \
+  -v $cfg_path/conf.d/:/etc/nginx/conf.d/:ro \
   --name nginx-vagrant-boxes nginx
